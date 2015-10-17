81797c6a1ae08c6315a2928f13283c6eff4a7fdc
Philip Hofer <phofer@umich.edu>
2015-10-16 18:08:34 -0700

    asdf2

--- context.c
+++ context.c
@@ -103,18 +103,6 @@ static inline void swapctx(regctx_t *save, const regctx_t *jmp) {
 	return;
 }
 
-/* 
- * set up a ctx such that the next time
- * it is jumped into, func(udata) will
- * be called.
- */
-void setup(regctx_t *ctx, void (*func)(void*), void *udata, void *stack) {
-	setstack(ctx, (uintptr_t)stack);
-	setreturn(ctx, (uintptr_t)func);
-	setarg0(ctx, (uintptr_t)udata);
-	return;
-}
-
 /* possible task statuses */
 enum {
 	STATUS_EMPTY,    /* uninitialized */
@@ -132,22 +120,120 @@ struct tasklist_s {
 	task_t *tail;
 };
 
-/* the global run queue */
-struct{
-	tasklist_t queue;
+/* the global run queue/state */
+static struct{
+	task_t *running;
+	task_t t0;        /* the root task */
+	tasklist_t queue; /* runnable */
+	int parked;       /* count of parked tasks */
+	tasklist_t begin; /* blocking requests to newtask() */
 } runq;
 
 struct task_s {
-	void      *stack;
+	void      *stack;  /* stack base */
+	void      (start)(void*); 
+	void      *udata;  /* passed to task->start() */
 	task_t 	  *next;
-	task_t    *prev;
-	regctx_t   ctx;
-	int        status;
+	regctx_t   ctx;    /* saved register state, if not running */
+	int        status; /* STATUS_XXX */
 };
 
 #define MAXTASKS 1024
-#define MAXTASKWORDS MAXTASKS/(sizeof(uintptr_t)*8)
-task_t all_tasks[MAXTASKS];
+#define BITSLAB_SIZE MAXTASKS
+#define BITSLAB_TYPE task_t
+#define BITSLAB_TYPE_NAME taskslab_t
+#define BITSLAB_FN_PREFIX slab
+#include "bitslab.h"
+static taskslab_t tslab;
+#undef BITSLAB_SIZE
+#undef BITSLAB_TYPE
+#undef BITSLAB_TYPE_NAME
+#undef BITSLAB_FN_PREFIX
+
+
+/* task start */
+static void __entry(void) {
+	running->start(running->udata);
+	__exit();
+}
+
+/* task exit -- should never return */
+static void __exit(void) {
+	running->status = STATUS_EMPTY;
+	slab_free(&tslab, running);
+
+	/* if someone was waiting to
+	 * create a new task, mark them
+         * as runnable.
+	 */
+	if (runq.begin.top) {
+		task_t *c = list_pop(&runq.begin);
+		c->status = STATUS_RUNNABLE;
+		list_pushback(&runq.runnable, c);
+	}
+	
+	__jmpnext();
+	assert(0 && "unreachable");
+}
+
+void taskexit(void) { __exit(); }
+
+/* TODO: netpolling */
+static void netpoll(block int) { return; }
+
+/* jump to next free task */
+static void __jmpnext(void) {
+	task_t *next = list_pop(&runq.queue);
+	if (next == NULL && runq.parked) {
+		/* find work */
+		netpoll(1);
+		next = list_pop(&runq.queue);
+	}
+	/* there's no work left to do -- exit */
+	if (!next) exit(0);
+	
+	assert(top->status == STATUS_RUNNABLE);
+	running = top;
+	top->status = STATUS_RUNNING;
+	__loadctx(&top->ctx);
+}
+
+int yield();
+
+void wait(tasklist_t *l, task_t *t);
+
+void newtask(void (func)(void*), void *data) {
+	task_t *t;
+
+	/* 
+	 * we may need to wait 
+         * to allocate; there are
+	 * a fixed number of tasks.
+         */
+	if (runq.begin.top) {
+		wait(&runq.begin, running);	
+	}
+
+	t = slab_malloc(&tslab);
+	assert(t);
+	t->udata = data;
+	t->start = func;
+	t->status = STATUS_RUNNABLE;
+	setup(&t->ctx, stack);
+	list_pushback(&runq.runnable, t);
+	return;
+}
+
+/* 
+ * set up a ctx such that the next time
+ * it is jumped into, func(udata) will
+ * be called.
+ */
+void setup(regctx_t *ctx, void *stack) {
+	setstack(ctx, (uintptr_t)stack);
+	setreturn(ctx, (uintptr_t)__entry);
+	return;
+}
 
 task_t *list_pop(tasklist_t *tl) {
 	if (tl->top == NULL) {
@@ -155,26 +241,23 @@ task_t *list_pop(tasklist_t *tl) {
 	}
 	task_t *out = tl->top;
 	tl->top = out->next;
-	if (tl->top) {
-		tl->top->prev = NULL;
-	} else {
+	if (tl->top == NULL) {
 		assert(out == tl->tail);
 		tl->tail = NULL;
 	}
 	out->next = NULL;
-	out->prev = NULL;
 	return out;
 }
 
 void list_pushback(tasklist_t *tl, task_t *task) {
 	if (tl->top == NULL) {
+		assert(tl->tail == NULL);
 		tl->top = task;
 		tl->tail = task;
 		return;
 	}
 	assert(tl->tail);
 	tl->tail->next = task;
-	task->prev = tl->tail;
 	tl->tail = task;
 	task->next = NULL;
 	return;
@@ -185,6 +268,7 @@ int wake(tasklist_t *tl) {
 	task_t *task = list_pop(tl);
 	if (task) {
 		task->status = STATUS_RUNNABLE;
+		--runq.parked;
 		list_pushback(&runq.queue, task);
 		return 1;
 	}
@@ -211,6 +295,7 @@ static int yield(task_t* task) {
 		assert(other != task);
 		assert(other->status == STATUS_RUNNABLE);
 		other->status = STATUS_RUNNING;
+		runq.running = other;
 		swapctx(&task->ctx, &other->ctx);
 		return 1;
 	}
@@ -221,9 +306,25 @@ static int yield(task_t* task) {
 /* park task on tasklist; deschedule */
 void wait(tasklist_t *tl, task_t *task) {
 	task->status = STATUS_PARKED;
+	++runq.parked
 	list_pushback(tl, task);
 	if (yield(task) == 0) assert(0 && "deadlock");
-	/* make sure we were woken by another yield() */
+
+	/* make sure we were woken appropriately */
 	assert(task->status == STATUS_RUNNING);
+	assert(runq.running == task)
 	return;
+}
+
+/* main */
+extern int taskmain();
+
+int main(void) {
+	/* 
+	 * setup t0, which
+	 * runs on the system stack.
+	 */
+	t0.status = STATUS_RUNNING;
+	runq.running = &runq.t0;
+	return taskmain();
 }
