c88bda653aba8d167bbf36ac81c82f133ee074d1
Philip Hofer <phofer@umich.edu>
2015-11-16 19:24:20 -0800

    test many stacks

---
+++ src/tests/many_test.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <assert.h>
+#include "../chip.h"
+
+/* 
+ * spawn() this many tasks that all
+ * contend on a lock, which exercises
+ * task heap allocation/deallocation.
+ * This is essentially worst-case behavior
+ * in terms of pressure to allocate many
+ * tasks.
+ */
+#define INCS 3000
+
+static int count;
+static sema_t sema;   /* 'done' semaphore */
+static mutex_t ilock; /* lock to force many tasks to exist */
+
+static void inc(void *data) {
+	lock(&ilock);
+	if (++count == INCS) {
+		post(&sema);
+	}
+        unlock(&ilock);
+	return;
+}
+
+int main(void) {
+	puts("running "__FILE__);
+	lock(&ilock);
+
+        /* 
+	   with the lock held, start a bunch of tasks 
+	   that contend on the lock.
+	 */
+	for (int i=0; i<INCS; ++i) {
+		spawn(inc, NULL);
+	}
+
+	tsk_stats_t stats;
+	get_tsk_stats(&stats);
+	printf("after %d spawns, %d parked, %d free, %d runnable; %d arenas\n", INCS, stats.parked, stats.free, stats.runnable, stats.arenas);
+
+	/* there should be INCS many pending tasks */
+	assert(stats.parked+stats.runnable == INCS);
+	
+        unlock(&ilock);
+
+	park(&sema);
+
+	/* we waited for everything to end -- make sure this is the case. */
+	get_tsk_stats(&stats);
+	assert(stats.runnable == 0);
+	assert(stats.parked == 0);
+
+	printf("after unlock, %d parked, %d free, %d runnable; %d arenas\n", stats.parked, stats.free, stats.runnable, stats.arenas);
+	
+	assert(sema.count == 0);
+	assert(count == INCS);
+	puts(__FILE__ " passed.");
+	return 0;
+}
